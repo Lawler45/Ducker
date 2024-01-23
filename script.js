301a6234a37f270dd05196c15fa556a605fc8378
Lewis Lawler <lewislawler98@gmail.com>
2024-01-23 13:41:14 +0000

    adds grid and inital variables

---
+++ script.js
@@ -0,0 +1,40 @@
+//Select Html elements
+
+const grid = document.querySelector(".grid");
+const timer = document.querySelector(".timer");
+const endGameScreen = document.querySelector(".end-game-screen");
+const endGameText = document.querySelector(".end-game-text");
+const playAgainBtn = document.querySelector(".play-again");
+
+const gridMatrix = [
+  ["", "", "", "", "", "", "", "", ""],
+  [
+    "river",
+    "wood",
+    "wood",
+    "river",
+    "wood",
+    "river",
+    "river",
+    "river",
+    "river",
+  ],
+  ["river", "river", "river", "wood", "wood", "river", "wood", "wood", "river"],
+  ["", "", "", "", "", "", "", "", ""],
+  ["road", "bus", "road", "road", "road", "car", "road", "road", "road"],
+  ["road", "road", "road", "car", "road", "road", "road", "road", "bus"],
+  ["road", "road", "car", "road", "road", "road", "bus", "road", "road"],
+  ["", "", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", "", ""],
+];
+
+//Initialise variables that control the game
+const victoryRow = 0;
+const riverRows = [1, 2];
+const roadRows = [4, 5, 6];
+const duckPosition = { x: 4, y: 8 };
+let contentBeforeDuck = "";
+
+function drawGrid() {
+  grid.innerHTML = "";
+}
